b46fb552f004e6e8e2b83da0b41d3e2608919b9d
Mark Wallace <lotabout@gmail.com>
2016-09-19 09:04:25 +0800

    [reader] handle quotes for regexp reader

--- regex-reader.rkt
+++ regex-reader.rkt
@@ -3,6 +3,9 @@
 (provide (rename-out [/-read read]
                      [/-read-syntax read-syntax]))
 
+(module+ test
+  (require rackunit))
+
 (define (/-read in)
   (read in))
 
@@ -22,14 +25,14 @@
     (bytes->string/locale
       (cadr (regexp-match #px"((?:\\\\.|(?<!\\\\).)*?(?<!\\\\))/" in))))
   (datum->syntax #f
-                 (pregexp (escape-backslash regex-expr))
+                 (pregexp (escape regex-expr))
                  (vector src line col pos (string-length regex-expr))))
 
 (define (make-/-readtable)
   (make-readtable (current-readtable)
                   #\/ 'dispatch-macro read-/))
 
-;;; wrap-backslash: helper function to escape backslash if "\\<char>" can be recognized by racket
+;;; wrap-escape: helper function to escape backslash if "\\<char>" can be recognized by racket
 ;;; reader
 ;;;
 ;;; "\\d\\t" -> "\"\\\\d\\t\""
@@ -38,7 +41,7 @@
 ;;; 2. "\\t" can be parsed by racket reader, keep it as original: "\\t"
 ;;; 3. wrap the whole result with quotes, resulting: "\"\\\\d\\t\""
 ;;; It will later be passed for `read`
-(define (wrap-backslash str)
+(define (wrap-escape str)
   (define in (open-input-string str))
   (let loop ([ch (read-char in)] [ret '(#\")])
     (if (eof-object? ch)
@@ -47,26 +50,43 @@
           [(eq? ch #\\)
            (define next (peek-char in))
            (cond
-             [(char=? next #\\)
-              ; consume '\\'
+             [(eqv? next #\\) ; consume '\\'
               (read-char in)
               (loop (read-char in) (cons #\\ (cons #\\ ret)))]
-             [(ormap (lambda (ch) (char=? ch next))
-                      (list #\a #\b #\t #\n #\v #\f #\r #\e #\" #\' #\x #\u #\U #\newline #\linefeed
+             [(ormap (lambda (ch) (eqv? ch next))
+                      (list #\a #\b #\t #\n #\v #\f #\r #\e #\x #\u #\U #\newline #\linefeed
                             #\return))
               ; leave it as it is
               (loop (read-char in) (cons #\\ ret))]
              [else
                ; escape character '\'
                (loop (read-char in) (cons #\\ (cons #\\ ret)))])]
+          [(eqv? ch #\")
+           (loop (read-char in) (cons #\" (cons #\\ ret)))]
           [else (loop (read-char in) (cons ch ret))]))))
+
+(module+ test
+  (check-equal? (wrap-escape "\\a\\b\\t\\n\\v\\f\\r\\e\\x\\u\\U")
+                "\"\\a\\b\\t\\n\\v\\f\\r\\e\\x\\u\\U\"")
+  (check-equal? (wrap-escape "escaped quotes: \\\"\\'")
+                "\"escaped quotes: \\\\\\\"\\\\'\"")
+  (check-equal? (wrap-escape "\\d\\1") "\"\\\\d\\\\1\"")
+  (check-equal? (wrap-escape "double quote: \"") "\"double quote: \\\"\""))
 
 ;;; escape the backslash('\') in a string for regexp
 ;;;
-;;; escape-backslash will parse the escapped character in a raw string in the same way the racket
+;;; escape will parse the escapped character in a raw string in the same way the racket
 ;;; reader handles strings
 ;;;
 ;;; "\\d\\t" -> "\\d\t"
 ;;; because "\\t" can be read by racket reader into string "\t", however "\\d" is not recognized.
-(define (escape-backslash str)
-  (read (open-input-string (wrap-backslash str))))
+(define (escape str)
+  (read (open-input-string (wrap-escape str))))
+
+(module+ test
+  (check-equal? (escape "\\a\\b\\t\\n\\v\\f\\r\\e\\x64\\u4e2d\\U4e2d")
+                "\a\b\t\n\v\f\r\e\x64\u4e2d\U4e2d")
+  (check-equal? (escape "escaped quotes: \\\"\\'")
+                "escaped quotes: \\\"\\'")
+  (check-equal? (escape "\\d\\1") "\\d\\1")
+  (check-equal? (escape "double quote: \"") "double quote: \""))
