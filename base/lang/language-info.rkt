10882fcc441fa608d22975c79fce76f7daf20dac
Mark Wallace <lotabout@gmail.com>
2016-09-29 19:04:31 +0800

    Revert "[base] make reader macro hygienic"
    This reverts commit f341936e271c7ee3e22d1416787af8dbf4c17965.

--- base/lang/language-info.rkt
+++ base/lang/language-info.rkt
@@ -8,20 +8,3 @@
       [(configure-runtime)
        '(#[axe/base/lang/runtime-config configure #f])]
       [else default])))
-
-;(require racket/match)
-
-;(define (get-language-info data)
-  ;(define other-get-info
-    ;(match data
-      ;[(vector mod sym data2)
-       ;((dynamic-require mod sym) data2)]
-      ;[_ (lambda (key default) default)]))
-  ;(lambda (key default)
-    ;(case key
-      ;[(configure-runtime)
-       ;(define config-vec '#[axe/base/lang/runtime-config configure #f])
-       ;(define other-config (other-get-info key default))
-       ;(cond [(list? other-config) (cons config-vec other-config)]
-             ;[else (list config-vec)])]
-      ;[else (other-get-info key default)])))
